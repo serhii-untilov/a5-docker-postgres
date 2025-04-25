15ade860f8fe378fe059de2d4d1a2e4ddac720b8
Serhii Untilov <s.untilov@gmail.com>
2025-04-25 07:00:54 +0300

    chore: minor update

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if ! [ -d ./postgres_data ]; then
-    echo "The ./postgres_data folder does not exist. Let's create it."
-    mkdir postgres_data
+echo "Make the config/*.conf files are readable."
+chmod 644 config/*.conf
 
-    echo "Make the config/*.conf files are readable."
-    chmod 644 config/*.conf
-fi
+# if ! [ -d ./postgres_data ]; then
+#     echo "The ./postgres_data folder does not exist. Let's create it."
+#     mkdir postgres_data
+# fi
 
-if ! [ -d ./postgres_backup ]; then
-    echo "The ./postgres_backup folder does not exist. Let's create it."
-    mkdir postgres_backup
-fi
+# if ! [ -d ./postgres_backup ]; then
+#     echo "The ./postgres_backup folder does not exist. Let's create it."
+#     mkdir postgres_backup
+# fi
 
 docker compose rm --force --stop --volumes
 docker compose up --detach --renew-anon-volumes --timestamps --remove-orphans
